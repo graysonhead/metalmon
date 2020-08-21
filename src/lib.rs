3c9cf01279d186eb99ab8f94b0878fe566e1a8e8
graysonhead <grayson@graysonhead.net>
2020-08-21 16:08:29 -0500

    Adding Integration Tests

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+// use diesel::prelude::*;
+pub mod database;
+pub mod schema;
+pub mod models;
---
+++ tests/integration_tests.rs
@@ -0,0 +1,23 @@
+use metalmon;
+
+#[cfg(test)]
+mod integration_tests {
+    use super::*;
+    #[test]
+    fn db_connection_test() {
+        let conn = metalmon::database::establish_connection();
+    }
+
+    #[test]
+    fn db_create_and_delete_user() {
+        let connection = metalmon::database::establish_connection();
+        let username = String::from("testuser");
+        let password = String::from("password");
+        let user = metalmon::database::create_user(&connection, &username, &password);
+        let num_deleted = metalmon::database::delete_user(&connection, &username);
+        assert_eq!(num_deleted, 1);
+    }
+}
+
+
+
